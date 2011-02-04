74a511431417fabe2f9e20ed2c0f0f6a20a1b303
Aaron Baldwin <baldwina@wwidea.org>
2011-02-04 13:27:57 -0700

    get tests running under rails 2.3.10

---
+++ test/test_helper.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require 'rubygems'
+gem 'activerecord', '= 2.3.10'
+gem 'actionpack', '= 2.3.10'
+require 'sqlite3'
+require 'active_record'
+require 'action_controller'
+require 'action_controller/test_process'
+ActionController::Routing::Routes.reload rescue nil
+RAILS_ROOT = File.dirname(__FILE__)
+$:.unshift File.join(RAILS_ROOT, '/../lib')
+require 'princess'
+require "#{File.dirname(__FILE__)}/../init"
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+RAILS_DEFAULT_LOGGER = Logger.new(File.join(RAILS_ROOT, '/log/test.log'))
+RAILS_DEFAULT_LOGGER.level = Logger::DEBUG
+ActiveRecord::Base.logger = RAILS_DEFAULT_LOGGER
+ActionController::Base.logger = RAILS_DEFAULT_LOGGER
+RAILS_ENV = 'test'
