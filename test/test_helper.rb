96a77f341e9973e8c3dc959fbc49a6dda7891c87
Aaron Baldwin <baldwina@wwidea.org>
2011-02-04 15:01:43 -0700

    load rails 3 gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 require 'test/unit'
 require 'rubygems'
-gem 'activerecord', '= 2.3.10'
-gem 'actionpack', '= 2.3.10'
+gem 'activerecord', '= 3.0.3'
+gem 'actionpack', '= 3.0.3'
 require 'sqlite3'
 require 'active_record'
 require 'action_controller'
-require 'action_controller/test_process'
+# require 'action_controller/test_process'
 ActionController::Routing::Routes.reload rescue nil
 RAILS_ROOT = File.dirname(__FILE__)
 $:.unshift File.join(RAILS_ROOT, '/../lib')
