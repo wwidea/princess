c2ba57c5932b6ed4e09121d3c30cc4bb3f1a3490
Jonathan Garvin <jgarvin@wwidea.org>
2012-10-03 14:30:26 -0600

    fix test

--- test/erb_views_test.rb
+++ test/erb_views_test.rb
@@ -4,32 +4,32 @@
 require 'test_helper'
 
 class ErbArticlesControllerTest < ActionController::TestCase
-  
+
   def setup
     prepop_db
   end
-  
-  
+
+
   # show
   test "should get show and return html" do
     get :show, :id => 1
     assert_response :success
     assert !@response.body.match(/^%PDF/)
   end
-  
+
   test "should generate pdf with no argugments" do
     get :show, :id => 1, :format => 'pdf'
     assert_response :success
     assert @response.body.match(/^%PDF/)
   end
-  
-  
+
+
   # custom_one
   test "should get alternate_custom_one template" do
     get :custom_one, :id => 1
     assert_select 'h1', 'Article1'
     get :custom_one, {:id => 1, :format => 'pdf'}
     assert @response.body.match(/^%PDF/)
-    assert(@response.body.length > 30000)
+    assert(@response.body.length > 10000)
   end
 end
